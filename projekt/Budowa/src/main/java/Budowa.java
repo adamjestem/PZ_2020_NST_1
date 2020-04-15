08998b88be8b5c813f54ee10724f721bac32e105
FilipAlbrycht <33802858+FilipAlbrycht@users.noreply.github.com>
2020-04-15 17:12:03 +0200

    Poczenie z baza danych (#19)
    * Poczenie z baza danych
    
    * Poczenie z baza danych
    
    * Poczenie z baza danych

--- projekt/Budowa/src/main/java/Budowa.java
+++ projekt/Budowa/src/main/java/Budowa.java
@@ -1,9 +1,22 @@
+import database.DbConnector;
+import entities.Role;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
 
 public class Budowa extends Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        DbConnector dbConnector = new DbConnector();
+        dbConnector.setSessionFactory();
+        SessionFactory sessionFactory = dbConnector.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        // \|/ do usunięcia w dalszych taskach - zostawiam narazie do testów żeby każdy mógł sobie sprawdzić połączenie
+        List<Role> roles = session.createQuery("from Roles", Role.class).getResultList();
+        System.out.println(roles.get(0).getRoleName());
         launch(args);
     }
 
