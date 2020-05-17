1966d3b56a4b76de69c6910c7db8659a65f923d8
monter08 <monter96@gmail.com>
2020-05-17 12:23:00 +0200

    remove unnecessary hibernate cfg (#84)
    * remove unnecessary hibernate cfg
    
    * retain parameter
    
    Co-authored-by: Monteiro <monteiro@iMac-Monteiro.local>

--- projekt/Budowa/src/main/java/module-info.java
+++ projekt/Budowa/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module org.budowa {
     requires javafx.fxml;
     requires java.persistence;
     requires java.sql;
-    requires org.hibernate.orm.core;
     requires java.naming;
 
     requires net.bytebuddy;
---
+++ projekt/Budowa/src/test/java/AuthServiceTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+import org.budowa.services.AuthService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+//    private final AuthService authService;
+
+    @Test
+    public void login(){
+        AuthService authService = new AuthService();
+    }
+}
